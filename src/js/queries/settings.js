8eb7ddaa20324f058c4c170703e16e758f105d1e
Scott Geiger <geigers@binghamton.edu>
2024-04-05 10:26:38 -0400

    formatting change

--- src/js/queries/settings.js
+++ src/js/queries/settings.js
@@ -1,10 +1,16 @@
 import q from '../queries';
-import {useQuery,useMutation} from "react-query";
+import { useQuery, useMutation } from "react-query";
+import { defaultVals } from '../config/settings';
 
 export default function useSettingsQueries() {
     const getSettings = (...args) => {
         const options = args[0]?.options||args[0]||{};
-        return useQuery('settings',q('settings'),{staleTime:Infinity,cacheTime:Infinity,retry:false,...options});
+        return useQuery('settings',q('settings'),{
+            staleTime:Infinity,
+            cacheTime:Infinity,
+            retry:false,
+            ...options
+        });
     }
     const putSettings = () => useMutation(d=>q('settings','PUT',d)());
 
