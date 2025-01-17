5b6bcba95bc66ba3cecf46faa0425f2b6b2a16f1
Scott Geiger <geigers@binghamton.edu>
2025-01-17 15:06:54 -0500

    small screen and print formatting

--- src/js/blocks/form/review/review-employment-volunteer.js
+++ src/js/blocks/form/review/review-employment-volunteer.js
@@ -13,39 +13,39 @@ export default function ReviewEmploymentVolunteer() {
                 <Col as="h5">Volunteer</Col>
             </Row>
             <Row as="dl" className="mb-0">
-                <Col as="dt" md={2} className="mb-0">Sub-Role:</Col>
-                <Col as="dd" md={4} className="mb-0">{info.subRole?.label}</Col>
-                <Col as="dt" md={2} className="mb-0">Start Date:</Col>
-                <Col as="dd" md={4} className="mb-0"><DateFormat>{info.startDate}</DateFormat></Col>
-                <Col as="dt" md={2} className="mb-0">End Date:</Col>
-                <Col as="dd" md={4} className="mb-0"><DateFormat>{info.endDate}</DateFormat></Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Sub-Role:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{info.subRole?.label}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Start Date:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0"><DateFormat>{info.startDate}</DateFormat></Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">End Date:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0"><DateFormat>{info.endDate}</DateFormat></Col>
                 {info.subRole.id=='Instructor' &&
                     <>
-                        <Col as="dt" md={2} className="mb-0">Tenure Status:</Col>
-                        <Col as="dd" md={4} className="mb-0">{info.tenureStatus?.label}</Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Tenure Status:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">{info.tenureStatus?.label}</Col>
                     </>
                 }
-                <Col as="dt" md={2} className="mb-0">Hours/Week:</Col>
-                <Col as="dd" md={4} className="mb-0">{info.hoursPerWeek}</Col>
-                <Col as="dt" md={2} className="mb-0">Service Type:</Col>
-                <Col as="dd" md={4} className="mb-0">{info.serviceType?.label}</Col>
-                <Col as="dt" md={2} className="mb-0">Department:</Col>
-                <Col as="dd" md={4} className="mb-0">{info.department?.label}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Hours/Week:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{info.hoursPerWeek}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Service Type:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{info.serviceType?.label}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Department:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{info.department?.label}</Col>
                 {(info.subRole.id&&info.subRole.id.startsWith('CP')) && 
                     <>
-                        <Col as="dt" md={2} className="mb-0">Responsible Univ Official:</Col>
-                        <Col as="dd" md={4} className="mb-0">{info.univOfficial[0]?.label}</Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Responsible Univ Official:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">{info.univOfficial[0]?.label}</Col>
                     </>
                 }
                 {(info.subRole.id&&info.subRole.id.startsWith('CP')) && 
                     <>
-                        <Col as="dt" md={2} className="mb-0">Supervisor:</Col>
-                        <Col as="dd" md={4} className="mb-0">{info.supervisor[0]?.label}</Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Supervisor:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">{info.supervisor[0]?.label}</Col>
                     </>
                 }
                 <NewLine gap={2}/>
-                <Col as="dt" md={2} className="mb-0">Duties:</Col>
-                <Col as="dd" md={10} className="mb-0"><pre>{info.duties}</pre></Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Duties:</Col>
+                <Col as="dd" sm={9} md={10} className="mb-0"><pre>{info.duties}</pre></Col>
             </Row>
         </article>
     );
--- src/js/blocks/form/review/review-person-demographics.js
+++ src/js/blocks/form/review/review-person-demographics.js
@@ -13,46 +13,46 @@ export default function ReviewPersonDemographics() {
                 <Col as="h5">Demographics</Col>
             </Row>
             <Row as="dl" className="mb-0">
-                <Col as="dt" md={2} className="mb-0">Date of Birth:</Col>
-                <Col as="dd" md={4} className="mb-0"><DateFormat>{demo.birthDate}</DateFormat></Col>
-                <Col as="dt" md={2} className="mb-0">Legal Sex:</Col>
-                <Col as="dd" md={4} className="mb-0">{demo.GENDER.label}</Col>
-                <Col as="dt" md={2} className="mb-0">Gender Identity:</Col>
-                <Col as="dd" md={4} className="mb-0">{demo.GENDER_IDENTITY.label}</Col>
-                <Col as="dt" md={2} className="mb-0">US Citizen:</Col>
-                <Col as="dd" md={4} className="mb-0">{(demo.US_CITIZEN_INDICATOR=="Y")?"Yes":"No"}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Date of Birth:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0"><DateFormat>{demo.birthDate}</DateFormat></Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Legal Sex:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{demo.GENDER.label}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Gender Identity:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{demo.GENDER_IDENTITY.label}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">US Citizen:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{(demo.US_CITIZEN_INDICATOR=="Y")?"Yes":"No"}</Col>
                 {(demo.US_CITIZEN_INDICATOR!="Y")&&
                     <>
-                        <Col as="dt" md={2} className="mb-0">Non-US Citizen Type:</Col>
-                        <Col as="dd" md={4} className="mb-0">{Object.values(demo.NON_CITIZEN_TYPE).join(' - ')}</Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Non-US Citizen Type:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">{Object.values(demo.NON_CITIZEN_TYPE).join(' - ')}</Col>
                         {(demo.NON_CITIZEN_TYPE?.id=='OT')&&
                             <>
-                                <Col as="dt" md={2} className="mb-0">Emp Auth Card Only:</Col>
-                                <Col as="dd" md={4} className="mb-0">{(demo.EMP_AUTHORIZE_CARD_INDICATOR=="Y")?"Yes":"No"}</Col>
+                                <Col as="dt" sm={3} md={2} className="mb-0">Emp Auth Card Only:</Col>
+                                <Col as="dd" sm={9} md={4} className="mb-0">{(demo.EMP_AUTHORIZE_CARD_INDICATOR=="Y")?"Yes":"No"}</Col>
                             </>
                         }
-                        <Col as="dt" md={2} className="mb-0">Country of Citizenship:</Col>
-                        <Col as="dd" md={4} className="mb-0">{demo.CITIZENSHIP_COUNTRY_CODE.label}</Col>
-                        <Col as="dt" md={2} className="mb-0">Visa Type:</Col>
-                        <Col as="dd" md={4} className="mb-0">{demo.VISA_CODE.label}</Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Country of Citizenship:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">{demo.CITIZENSHIP_COUNTRY_CODE.label}</Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Visa Type:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">{demo.VISA_CODE.label}</Col>
                     </>
                 }
                 <NewLine gap={3}/>
-                <Col as="dt" md={2} className="mb-0">Military Status:</Col>
-                <Col as="dd" md={4} className="mb-0">
+                <Col as="dt" sm={3} md={2} className="mb-0">Military Status:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">
                     {demo.militaryStatus.map(s=><p key={s[0]} className="mb-0">{s[1]}</p>)}
                 </Col>
                 <NewLine gap={3}/>
-                <Col as="dt" md={2} className="mb-0">Veteran:</Col>
-                <Col as="dd" md={4} className="mb-0">{(demo.VETERAN_INDICATOR=="Y")?"Yes":"No"}</Col>
+                <Col as="dt" sm={3} md={2} className="mb-0">Veteran:</Col>
+                <Col as="dd" sm={9} md={4} className="mb-0">{(demo.VETERAN_INDICATOR=="Y")?"Yes":"No"}</Col>
                 {(demo.VETERAN_INDICATOR=="Y")&&
                     <>
-                        <Col as="dt" md={2} className="mb-0">Protected Vet Status:</Col>
-                        <Col as="dd" md={4} className="mb-0">
+                        <Col as="dt" sm={3} md={2} className="mb-0">Protected Vet Status:</Col>
+                        <Col as="dd" sm={9} md={4} className="mb-0">
                             {demo.protectedVetStatus.map(s=><p key={s[0]} className="mb-0">{s[1]}</p>)}
                         </Col>
-                        <Col as="dt" md={2} className="mb-0">Military Sep Date:</Col>
-                        <Col as="dd" md={4} className="mb-0"><DateFormat>{demo.militarySepDate}</DateFormat></Col>
+                        <Col as="dt" sm={3} md={2} className="mb-0">Military Sep Date:</Col>
+                        <Col as="dd" sm={3} md={4} className="mb-0"><DateFormat>{demo.militarySepDate}</DateFormat></Col>
                     </>
                 }
             </Row>
