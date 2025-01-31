2294bc4cb2c2e215f919d35aeb2a87f391000b80
Scott Geiger <geigers@binghamton.edu>
2025-01-31 08:57:46 -0500

    Add login history table

--- src/js/pages/login-history.js
+++ src/js/pages/login-history.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { Row, Col, Alert } from "react-bootstrap";
+import useSessionQueries from "../queries/session";
+import DataTable from "react-data-table-component";
+import { fromUnixTime, format, formatDistance } from "date-fns";
 
 export default function LoginHistory() {
+    const { getLoginHistory } = useSessionQueries();
+    const loginhistory = getLoginHistory();
+
+    const columns = [
+        {id:'login_date',name:'Login Date',selector:row=>row.LOGIN_DATE,sortable:true,format:row => {
+            const d = fromUnixTime(row.LOGIN_DATE);
+            const fmt = format(d,'Pp');
+            const dist = formatDistance(d,new Date(),{addSuffix:true});
+            return `${fmt} (${dist})`;
+        }},
+        {id:'ip_address',name:'IP Address',selector:row=>row.IP_ADDRESS,sortable:true},
+        {id:'user_agent',name:'User Agent',selector:row=>row.USER_AGENT,sortable:false,grow:2}
+    ];
+
     return (
         <section>
             <header>
@@ -10,7 +27,18 @@ export default function LoginHistory() {
                 </Row>
             </header>
             <article>
-                <Alert variant="warning">Page currently under construction - coming soon...</Alert>
+                <DataTable
+                    defaultSortFieldId="login_date"
+                    defaultSortAsc={false}
+                    className="compact"
+                    columns={columns} 
+                    data={loginhistory.data}
+                    progressPending={loginhistory.isLoading}
+                    pagination 
+                    striped 
+                    responsive
+                    highlightOnHover
+                />
             </article>
         </section>
     );
