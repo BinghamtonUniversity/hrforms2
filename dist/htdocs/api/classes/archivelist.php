bf61b51323e379873d4bafd9f447bd3e41403958
Scott Geiger <geigers@binghamton.edu>
2025-05-14 16:05:09 -0400

    Fix duplicate record problem in archivelist

--- dist/htdocs/api/classes/archivelist.php
+++ dist/htdocs/api/classes/archivelist.php
@@ -85,7 +85,7 @@ class ArchiveList extends HRForms2 {
 						rank() over (partition by jr1.request_id order by jr1.journal_date desc) as rnk
 						from hrforms2_requests_journal_archive jr1
 					) jr2
-					where jr2.sequence >= 0
+					where jr2.sequence >= 0 and jr2.rnk = 1
 					and jr2.group_to in (select group_id from hrforms2_user_groups where suny_id = :suny_id)) j
 					left join (select request_id, max(journal_date) as max_journal_date, listagg(group_to,',') as journal_groups, listagg(status,',') within group (order by sequence) as journal_status from hrforms2_requests_journal_archive where sequence >= 0 group by request_id) js on (js.request_id = j.request_id)
 					where r.request_id = j.request_id
@@ -121,7 +121,7 @@ class ArchiveList extends HRForms2 {
 						rank() over (partition by jr1.request_id order by jr1.journal_date desc) as rnk
 						from hrforms2_requests_journal_archive jr1
 					) jr2
-					where jr2.sequence >= 0
+					where jr2.sequence >= 0 and jr2.rnk = 1
 					and jr2.group_to in (select group_id from hrforms2_user_groups where suny_id = :suny_id)) j
 					left join (select request_id, max(journal_date) as max_journal_date, listagg(group_to,',') as journal_groups, listagg(status,',') within group (order by sequence) as journal_status from hrforms2_requests_journal_archive where sequence >= 0 group by request_id) js on (js.request_id = j.request_id)
 					where r.request_id = j.request_id
@@ -190,7 +190,7 @@ class ArchiveList extends HRForms2 {
 						rank() over (partition by jf1.form_id order by jf1.journal_date desc) as rnk
 						from hrforms2_forms_journal_archive jf1
 					) jf2
-					where jf2.sequence >= 0
+					where jf2.sequence >= 0 and jf2.rnk = 1
 					and jf2.group_to in (select group_id from hrforms2_user_groups where suny_id = :suny_id)) j
 					left join (select form_id, max(journal_date) as max_journal_date, listagg(group_to,',') as journal_groups, listagg(status,',') within group (order by sequence) as journal_status from hrforms2_forms_journal_archive where sequence >= 0 group by form_id) js on (js.form_id = j.form_id)
 					where f.form_id = j.form_id
@@ -245,7 +245,7 @@ class ArchiveList extends HRForms2 {
 						rank() over (partition by jf1.form_id order by jf1.journal_date desc) as rnk
 						from hrforms2_forms_journal_archive jf1
 					) jf2
-					where jf2.sequence >= 0
+					where jf2.sequence >= 0 and jf2.rnk = 1
 					and jf2.group_to in (select group_id from hrforms2_user_groups where suny_id = :suny_id)) j
 					left join (select form_id, max(journal_date) as max_journal_date, listagg(group_to,',') as journal_groups, listagg(status,',') within group (order by sequence) as journal_status from hrforms2_forms_journal_archive where sequence >= 0 group by form_id) js on (js.form_id = j.form_id)
 					where f.form_id = j.form_id
