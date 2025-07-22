2cf5bbe372fe5ffcbf88c757ab7bd26a875f638f
Scott Geiger <geigers@binghamton.edu>
2025-07-22 15:09:56 -0400

    Added version checking and cache clearing on change

--- src/js/queries.js
+++ src/js/queries.js
@@ -13,6 +13,33 @@ export function qErr(key,options={},message='Bad Data') {
     return useQuery(key,()=>new Promise((resolve,reject)=>reject(message)),options);
 }
 
+function checkVersion(headers) {
+    const version = headers.get('X-App-Version');
+    const revision = headers.get('X-App-Revision');
+    const localVersion = localStorage.getItem('version');
+    const localRevision = localStorage.getItem('revision');
+    if (localVersion === null && localRevision === null) {
+        // if localStorage is not set assume new device and set the version and revision
+        localStorage.setItem('version',version);
+        localStorage.setItem('revision',revision);
+    } else if (localVersion != version || localRevision != revision) {
+        // if the version or revision has changed, clear the cache and reload the page
+        if ('cache' in window) {
+            caches.keys().then((names) => {
+                names.forEach((name) => {
+                    caches.delete(name).then(() => {
+                        console.debug(`Cleared cache: ${name}`);
+                    });
+                });
+            });
+        }
+        //localStorage.clear();
+        localStorage.setItem('version',version);
+        localStorage.setItem('revision',revision);
+        window.location.reload(true); // Force reload the page to apply changes
+    }
+}
+
 /* BASE PROMISE */
 export default function q(u,m,b) {
     return d => {
@@ -28,7 +55,10 @@ export default function q(u,m,b) {
         return new Promise((res,rej) => {
             //TODO: check window.sessionStorage; combine with cookie to handle expiration.
             Promise.race([
-                fetch(`/api/api.php/${u}`,opts).then(r=>(!r.ok)?rej({name:r.status,message:r.statusText,description:r.headers.get('X-Error-Description')}):r.json()),
+                fetch(`/api/api.php/${u}`,opts).then(r=>{
+                    checkVersion(r.headers);
+                    return (!r.ok)?rej({name:r.status,message:r.statusText,description:r.headers.get('X-Error-Description')}):r.json()
+                }),
                 new Promise((resolve,reject)=>setTimeout(reject,requestTimeout,{status:408,name:'408',message:'Request Timeout'}))
             ]).then(j=>{
                 //TODO: if storable; sessionStorage.setItem('key','value') <-- value needs to be stringified json.
