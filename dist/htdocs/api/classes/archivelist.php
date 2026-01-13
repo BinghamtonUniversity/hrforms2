af30d42b6a7d4a87c53da82dab8b5c994c4a97b1
Scott Geiger <geigers@binghamton.edu>
2026-01-13 10:52:00 -0500

    Changes to support updated by field in archive and fixes to search for created by and updated by

--- dist/htdocs/api/classes/archivelist.php
+++ dist/htdocs/api/classes/archivelist.php
@@ -69,6 +69,7 @@ class ArchiveList extends HRForms2 {
                     "multiLines"=>"N",
                     "title"=>"",
                     "createdBy"=>"",
+                    "updatedBy"=>"",
                     "sortField"=>"request_id",
                     "sortDir"=>"desc"
                 ),$_GET);
@@ -84,13 +85,19 @@ class ArchiveList extends HRForms2 {
                     if ($_GET['multiLines'] == "Y") $filter .= " and r.request_data.multiLines = 'Y'";
                     if ($_GET['title'] != "") $filter .= " and lower(r.request_data.reqBudgetTitle.title) like lower('%'||:title||'%')";
                     if ($_GET['createdBy'] != "") $filter .= " and r.created_by.SUNY_ID = :created_by";
+                    if ($_GET['updatedBy'] != "") $filter .= " and j.suny_id = :updated_by";
                 }
                 $sort = "";
                 switch($_GET['sortField']) {
                     case "candidate_name":
                         $sort = "order by LEGAL_LAST_NAME || ',' || FIRST_NAME " . $_GET['sortDir'] . ", effdate desc";
                         break;
-                    
+                    case "created_by":
+                        $sort = "order by r.created_by.LEGAL_LAST_NAME || ',' || r.created_by.ALIAS_FIRST_NAME || ',' || r.created_by.LEGAL_FIRST_NAME " . $_GET['sortDir'] . ", effdate desc";
+                        break;
+                    case "last_updated_by":
+                        $sort = "order by trim(u.last_update_last_name || ',' || u.last_update_first_name)" . $_GET['sortDir'] . ", effdate desc";
+                        break;                    
                     default:
                         $sort = "order by " . $_GET['sortField'] . " " . $_GET['sortDir'];
                 }
@@ -125,6 +132,7 @@ class ArchiveList extends HRForms2 {
                     if ($_GET['lineNumber'] != "") oci_bind_by_name($stmt,":lineNumber",$_GET['lineNumber']);
                     if ($_GET['title'] != "") oci_bind_by_name($stmt,":title",$_GET['title']);
                     if ($_GET['createdBy'] != "") oci_bind_by_name($stmt,":created_by",$_GET['createdBy']);
+                    if ($_GET['updatedBy'] != "") oci_bind_by_name($stmt,":updated_by",$_GET['updatedBy']);
                 }
                 $r = oci_execute($stmt);
                 if (!$r) $this->raiseError();
@@ -139,11 +147,13 @@ class ArchiveList extends HRForms2 {
                     r.request_data.posType, r.request_data.reqType, r.request_data.effDate, 
                     r.request_data.candidateName, r.request_data.multiLines, r.request_data.lineNumber, 
                     r.request_data.reqBudgetTitle.title, 
-                    nvl(r.created_by.ALIAS_FIRST_NAME,r.created_by.LEGAL_FIRST_NAME) as FIRST_NAME, 
-                    r.created_by.LEGAL_LAST_NAME, 
+                    nvl(r.created_by.ALIAS_FIRST_NAME,r.created_by.LEGAL_FIRST_NAME) as created_by_first_name, 
+                    r.created_by.LEGAL_LAST_NAME as created_by_legal_last_name, 
                     to_char(r.created_date,'DD-MON-YYYY HH24:MI:SS') as created_date, 
                     r.request_data.WORKFLOW_ID,
-                    j.status, j.sequence, js.journal_groups as groups, js.journal_status,
+                    j.status, j.sequence, j.suny_id as last_updated_suny_id, 
+                    trim(u.last_update_first_name || ' ' || u.last_update_last_name) as last_updated_name,
+                    js.journal_groups as groups, js.journal_status,
                     to_char(js.max_journal_date,'DD-MON-YYYY HH24:MI:SS') as max_journal_date
                     from hrforms2_requests_archive r,
                     (select jr2.* from (select jr1.*,
@@ -152,6 +162,7 @@ class ArchiveList extends HRForms2 {
                     ) jr2
                     where jr2.sequence >= 0 and jr2.rnk = 1) j
                     left join (select request_id, max(journal_date) as max_journal_date, listagg(group_to,',') as journal_groups, listagg(status,',') within group (order by sequence) as journal_status from hrforms2_requests_journal_archive where sequence >= 0 group by request_id) js on (js.request_id = j.request_id)
+                    left join (select u1.suny_id, nvl(u1.user_info.ALIAS_FIRST_NAME,u1.user_info.LEGAL_FIRST_NAME) as last_update_first_name, u1.user_info.LEGAL_LAST_NAME as last_update_last_name from hrforms2_users u1) u on (u.suny_id = j.suny_id) 
                     where r.request_id = j.request_id
                     and exists (select 1
                         from hrforms2_requests_journal_archive
@@ -178,6 +189,7 @@ class ArchiveList extends HRForms2 {
                     if ($_GET['lineNumber'] != "") oci_bind_by_name($stmt,":lineNumber",$_GET['lineNumber']);
                     if ($_GET['title'] != "") oci_bind_by_name($stmt,":title",$_GET['title']);
                     if ($_GET['createdBy'] != "") oci_bind_by_name($stmt,":created_by",$_GET['createdBy']);
+                    if ($_GET['updatedBy'] != "") oci_bind_by_name($stmt,":updated_by",$_GET['updatedBy']);
                 }
                 oci_bind_by_name($stmt,":offset",$offset);
                 oci_bind_by_name($stmt,":results",$results);
@@ -204,6 +216,7 @@ class ArchiveList extends HRForms2 {
                     "lineNumber"=>"",
                     "benefitFlag"=>"",
                     "createdBy"=>"",
+                    "updatedBy"=>"",
                     "sortField"=>"form_id",
                     "sortDir"=>"desc"
                 ),$_GET);
@@ -223,6 +236,7 @@ class ArchiveList extends HRForms2 {
                     if ($_GET['lineNumber'] != "") $filter .= " and f.form_data.employment.position.positionDetails.LINE_NUMBER = :line_number";
                     if ($_GET['benefitFlag'] != "") $filter .= " and f.form_data.employment.position.BENEFIT_FLAG.id = :benefits_flag";
                     if ($_GET['createdBy'] != "") $filter .= " and f.created_by.SUNY_ID = :created_by";
+                    if ($_GET['updatedBy'] != "") $filter .= " and j.suny_id = :updated_by";
                 }
 
                 $sort = "";
@@ -232,6 +246,12 @@ class ArchiveList extends HRForms2 {
                         break;
                     case "form_code":
                         $sort = "order by FORM_CODE || '-' || ACTION_CODE || '-' || TRANSACTION_CODE " . $_GET['sortDir'] . ", effdate desc";
+                        break;
+                    case "created_by":
+                        $sort = "order by f.created_by.LEGAL_LAST_NAME || ',' || f.created_by.ALIAS_FIRST_NAME || ',' || f.created_by.LEGAL_FIRST_NAME " . $_GET['sortDir'] . ", effdate desc";
+                        break;
+                    case "last_updated_by":
+                        $sort = "order by trim(u.last_update_last_name || ',' || u.last_update_first_name)" . $_GET['sortDir'] . ", effdate desc";
                         break;
                     default:
                         $sort = "order by " . $_GET['sortField'] . " " . $_GET['sortDir'];
@@ -269,6 +289,7 @@ class ArchiveList extends HRForms2 {
                     if ($_GET['lineNumber'] != "") oci_bind_by_name($stmt,":line_number",$_GET['lineNumber']);
                     if ($_GET['benefitFlag'] != "") oci_bind_by_name($stmt,":benefits_flag",$_GET['benefitFlag']);
                     if ($_GET['createdBy'] != "") oci_bind_by_name($stmt,":created_by",$_GET['createdBy']);
+                    if ($_GET['updatedBy'] != "") oci_bind_by_name($stmt,":updated_by",$_GET['updatedBy']);
                 }
                 $r = oci_execute($stmt);
                 if (!$r) $this->raiseError();
@@ -298,7 +319,9 @@ class ArchiveList extends HRForms2 {
                     f.form_data.employment.position.positionDetails.LINE_NUMBER as LINE_NUMBER,
                     f.form_data.employment.position.BENEFIT_FLAG as BENEFIT_FLAG,
                     f.form_data.employment.position.positionDetails.TITLE as TITLE,
-                    j.status, j.sequence, js.journal_groups as groups, js.journal_status,
+                    j.status, j.sequence, j.suny_id as last_updated_suny_id, 
+                    trim(u.last_update_first_name || ' ' || u.last_update_last_name) as last_updated_name,
+                    js.journal_groups as groups, js.journal_status,
                     to_char(js.max_journal_date,'DD-MON-YYYY HH24:MI:SS') as max_journal_date
                     from hrforms2_forms_archive f,
                     (select jf2.* from (select jf1.*,
@@ -307,6 +330,7 @@ class ArchiveList extends HRForms2 {
                     ) jf2
                     where jf2.sequence >= 0 and jf2.rnk = 1) j
                     left join (select form_id, max(journal_date) as max_journal_date, listagg(group_to,',') as journal_groups, listagg(status,',') within group (order by sequence) as journal_status from hrforms2_forms_journal_archive where sequence >= 0 group by form_id) js on (js.form_id = j.form_id)
+                    left join (select u1.suny_id, nvl(u1.user_info.ALIAS_FIRST_NAME,u1.user_info.LEGAL_FIRST_NAME) as last_update_first_name, u1.user_info.LEGAL_LAST_NAME as last_update_last_name from hrforms2_users u1) u on (u.suny_id = j.suny_id) 
                     where f.form_id = j.form_id
                     and exists (select 1
                         from hrforms2_forms_journal_archive
@@ -334,6 +358,7 @@ class ArchiveList extends HRForms2 {
                         if ($_GET['lineNumber'] != "") oci_bind_by_name($stmt,":line_number",$_GET['lineNumber']);
                         if ($_GET['benefitFlag'] != "") oci_bind_by_name($stmt,":benefits_flag",$_GET['benefitFlag']);
                         if ($_GET['createdBy'] != "") oci_bind_by_name($stmt,":created_by",$_GET['createdBy']);
+                        if ($_GET['updatedBy'] != "") oci_bind_by_name($stmt,":updated_by",$_GET['updatedBy']);
                     }
                     oci_bind_by_name($stmt,":offset",$offset);
                     oci_bind_by_name($stmt,":results",$results);
