4a2f37096050a662d931a4217262378921f5e6c2
Scott Geiger <geigers@binghamton.edu>
2026-02-26 16:33:14 -0500

    formatting fix, footer causing small horizontal scroll

--- src/js/blocks/footer.js
+++ src/js/blocks/footer.js
@@ -11,20 +11,20 @@ export default function Footer() {
     useEffect(()=>setCurrYear(new Date().getFullYear()),[]);
     return (
         <footer className="mt-4 mb-2">
-            <Row className={((INSTANCE!="PROD")?"bg-primary":"bg-main") + " text-white py-1 mb-1"}>
+            <Row className={((INSTANCE!="PROD")?"bg-primary":"bg-main") + " text-white py-1 mb-1 mr-0"}>
                 <Col className="d-flex justify-content-center">&copy; 2021 - {currYear} Binghamton University</Col>
             </Row>
-            <Row>
+            <Row className="mr-0">
                 <Col className="d-flex justify-content-center"><small><Link to="/login-history">Last Login</Link>: <DateFormat inFmt="t" outFmt="E, MMM d, yyyy h:mm a" nvl={<span className="font-italic">Never</span>}>{LAST_LOGIN_DATE}</DateFormat> {LAST_IP_ADDRESS && `from ${LAST_IP_ADDRESS}`}</small></Col>
             </Row>
-            <Row>
+            <Row className="mr-0">
                 <Col className="d-flex justify-content-center"><small><Link to="/version-info">Version</Link>: {VERSION} ({REVISION})</small></Col>
             </Row>
-            <Row className="py-3 d-none d-print-block">
+            <Row className="py-3 d-none d-print-block mr-0">
                 <Col className="d-flex justify-content-center"><small>[{INSTANCE}]:{pathname}</small></Col>
             </Row>
             {DEBUG && 
-                <Row>
+                <Row className="mr-0">
                     <Col>
                         <p className="text-muted d-block d-sm-none">xs</p>
                         <p className="text-muted d-none d-sm-block d-md-none">sm</p>
