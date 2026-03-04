fa666fd2e6641bc6a3a1167f99643873bd62d366
Scott Geiger <geigers@binghamton.edu>
2026-03-04 10:03:17 -0500

    Added error handling for user groups for determining edit permission.

--- src/js/pages/request.js
+++ src/js/pages/request.js
@@ -444,7 +444,7 @@ function RequestForm({reqId,data,setIsBlocking,isDraft,isNew,reset,historyFrom})
         if (isDraft && SUNY_ID == get(data,'CREATED_BY_SUNY_ID',SUNY_ID)) return true;
         if (!isDraft && SUNY_ID == data.lastJournal.CREATED_BY_SUNY_ID && data.lastJournal.STATUS == 'R') return true;
         if (!isDraft && SUNY_ID == data.lastJournal.CREATED_BY_SUNY_ID && !requests.permissions.approveown) return false;
-        const userGroups = USER_GROUPS.split(',');
+        const userGroups = (!USER_GROUPS)?[]:USER_GROUPS.split(',');
         if (userGroups.includes(get(data,'lastJournal.GROUP_TO')) && data.lastJournal.STATUS != 'R') return true;
         return false;
     },[SUNY_ID,USER_GROUPS,data,isDraft]);
